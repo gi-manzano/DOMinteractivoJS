cf434adccf240baa95844f90cfc015007729300a
Gisele <manzanogisele85@gmail.com>
2022-02-11 23:52:32 -0300

    modificaciones

--- stock.js
+++ stock.js
@@ -1,8 +1,8 @@
 let stockProductos = [
     {id: 1, nombre: "Ingles Junior", tipo: "curso", desc: "Un curso ideal para vos", precio: 12000, Idioma: "Junior", img: '/assets/jason-goodman-Oalh2MojUuk-unsplash.jpg', cantidad:1},
-    {id: 2, nombre: "Ingles Basico", tipo: "curso", desc: "Un curso ideal para vos", precio: 11000, Idioma: "Basico", img: '/assets/scott-graham-5fNmWej4tAA-unsplash.jpg', cantidad:1},
+    {id: 2, nombre: "Ingles Básico", tipo: "curso", desc: "Un curso ideal para vos", precio: 11000, Idioma: "Básico", img: '/assets/scott-graham-5fNmWej4tAA-unsplash.jpg', cantidad:1},
     {id: 3, nombre: "Ingles Intermedio", tipo: "curso", desc: "Un curso ideal para vos", precio: 12000, Idioma: "Intermedio", img: '/assets/scott-graham-5fNmWej4tAA-unsplash.jpg', cantidad:1},
-    {id: 4, nombre: "Italiano Basico", tipo: "curso", desc: "Un curso ideal para vos", precio: 14000, Idioma: "Basico", img: '/assets/kobu-agency-7okkFhxrxNw-unsplash.jpg', cantidad:1},
+    {id: 4, nombre: "Italiano Básico", tipo: "curso", desc: "Un curso ideal para vos", precio: 14000, Idioma: "Básico", img: '/assets/kobu-agency-7okkFhxrxNw-unsplash.jpg', cantidad:1},
     {id: 5, nombre: "Frances Intermedio", tipo: "curso", desc: "Un curso ideal para vos", precio: 12000, Idioma: "Intermedio", img: '/assets/kobu-agency-7okkFhxrxNw-unsplash.jpg', cantidad:1},
-    {id: 6, nombre: "Portuguez Basico", tipo: "curso", desc: "Un curso ideal para vos", precio: 15000, Idioma: "Basico", img: '/assets/kobu-agency-7okkFhxrxNw-unsplash.jpg', cantidad:1},
+    {id: 6, nombre: "Portugues Básico", tipo: "curso", desc: "Un curso ideal para vos", precio: 15000, Idioma: "Básico", img: '/assets/kobu-agency-7okkFhxrxNw-unsplash.jpg', cantidad:1},
 ]
